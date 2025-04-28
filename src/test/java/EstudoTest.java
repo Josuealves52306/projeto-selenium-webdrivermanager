b257201c311321bec45ada26193e8b310a264a0c
Josue <josue_alves2010@hotmail.com>
2025-04-28 20:24:54 -0300

    Cadastrando e realizando o login

--- src/test/java/EstudoTest.java
+++ src/test/java/EstudoTest.java
@@ -5,11 +5,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-//import static org.junit.jupiter.api.AssertEquals.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 //CLASSE
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -25,12 +22,13 @@ public class EstudoTest {
     //ANTES DO TESTE
     @BeforeAll
 
-    public static void setUp(){
+    public static void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(9));
     }
+
     @BeforeEach
     public void inicioTesteUnico() {
         driver.get("https://front.serverest.dev/login");
@@ -40,7 +38,6 @@ public class EstudoTest {
     //DEPOIS DO TESTE
     @AfterAll
     public static void tearDown() {
-        System.out.println("Teste Finalizado");
         driver.quit();
     }
 
@@ -52,20 +49,20 @@ public class EstudoTest {
 
         driver.findElement(By.linkText("Cadastre-se")).click();
         driver.findElement(By.id("nome")).click();
-        driver.findElement(By.id("nome")).sendKeys("josue27");
-        driver.findElement(By.id("email")).sendKeys("josue_alves2027@hotmail.com");
+        driver.findElement(By.id("nome")).sendKeys("josue31");
+        driver.findElement(By.id("email")).sendKeys("josue_alves2031@hotmail.com");
         driver.findElement(By.id("password")).click();
-        driver.findElement(By.id("password")).sendKeys("56762191");
+        driver.findElement(By.id("password")).sendKeys("123");
 
-        assertEquals("Cadastrar", driver.findElement(By.cssSelector(".btn-primary")).getText(),"Texto esperado 'Cadastrar com sucesso', mas foi exivido outro texto");
+        assertEquals("Cadastrar", driver.findElement(By.cssSelector(".btn-primary")).getText(), "Texto esperado 'Cadastrar com sucesso', mas foi exivido outro texto");
 
         driver.findElement(By.cssSelector(".form-check")).click();
         driver.findElement(By.cssSelector(".btn-primary")).click();
-        assertEquals("Cadastro realizado com sucesso", driver.findElement(By.cssSelector("a.alert-link")).getText(),"Texto esperado 'Cadastro realizado com sucesso', mas foi exivido outro texto");
-
+        assertEquals("Cadastro realizado com sucesso", driver.findElement(By.cssSelector("a.alert-link")).getText(), "Texto esperado 'Cadastro realizado com sucesso', mas foi exivido outro texto");
 
 
     }
+
     //TESTE2
     @Test
     @Order(2)
@@ -74,20 +71,42 @@ public class EstudoTest {
 
         driver.findElement(By.linkText("Cadastre-se")).click();
         driver.findElement(By.id("nome")).click();
-        driver.findElement(By.id("nome")).sendKeys("josue27");
-        driver.findElement(By.id("email")).sendKeys("josue_alves2027@hotmail.com");
+        driver.findElement(By.id("nome")).sendKeys("josue31");
+        driver.findElement(By.id("email")).sendKeys("josue_alves2031@hotmail.com");
         driver.findElement(By.id("password")).click();
-        driver.findElement(By.id("password")).sendKeys("56762191");
+        driver.findElement(By.id("password")).sendKeys("123");
 
-        assertEquals("Cadastrar", driver.findElement(By.cssSelector(".btn-primary")).getText(),"Texto esperado 'Cadastrar com sucesso', mas foi exivido outro texto");
+        assertEquals("Cadastrar", driver.findElement(By.cssSelector(".btn-primary")).getText(), "Texto esperado 'Cadastrar com sucesso', mas foi exivido outro texto");
 
         driver.findElement(By.cssSelector(".form-check")).click();
         driver.findElement(By.cssSelector(".btn-primary")).click();
-        assertEquals("Este email já está sendo usado", driver.findElement(By.cssSelector("div.alert.alert-secondary.alert-dismissible > span")).getText(),"Texto esperado 'Cadastro realizado com sucesso', mas foi exivido outro texto");
-
+        assertEquals("Este email já está sendo usado", driver.findElement(By.cssSelector("div.alert.alert-secondary.alert-dismissible > span")).getText(), "Texto esperado 'Cadastro realizado com sucesso', mas foi exivido outro texto");
 
 
     }
 
+    @Test
+    @Order(3)
+    @DisplayName("Teste realizado com login já cadastrado")
+    public void testLoginComSucesso() {
+
+        driver.findElement(By.id("email")).sendKeys("josue_alves2031@hotmail.com");
+        driver.findElement(By.id("password")).sendKeys("123");
+        driver.findElement(By.cssSelector("[data-testid='entrar']")).click();
+        assertEquals("Este é meu sistema para administrar seu ecommerce.", driver.findElement(By.cssSelector("p.lead")).getText(), "Texto esperado 'Cadastro realizado com sucesso', mas foi exivido outro texto");
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Teste realizado com login de usuario não cadastrado")
+    public void testLoginDadosInvalidos() {
+
+        driver.findElement(By.id("email")).sendKeys("josue_alves2050@hotmail.com");
+        driver.findElement(By.id("password")).sendKeys("123");
+        driver.findElement(By.cssSelector("[data-testid='entrar']")).click();
+        assertEquals("Email e/ou senha inválidos", driver.findElement(By.cssSelector("div.alert.alert-secondary.alert-dismissible > span")).getText(), "Email e/ou senha inválidos");
+
+
+    }
 
 }
